3b5ec292de440d7f384a3e3ae98622535c5d912c
JPCRibeiro <jopedroribeiro9@gmail.com>
2026-09-19 15:24:01 -0300

    feat: setup S3 storage configuration and service

---
+++ src/main/java/br/app/visuwall/shared/config/storage/S3Service.java
@@ -0,0 +1,27 @@
+package br.app.visuwall.shared.config.storage;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    public void uploadBytes(String key, byte[] data, String contentType) {
+        s3Client.putObject(
+                PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(key)
+                        .contentType(contentType)
+                        .build(),
+                RequestBody.fromBytes(data));
+    }
+}
