968a1f48077b82979c5465aad5d4511aea8f5abe
JPCRibeiro <jopedroribeiro9@gmail.com>
2026-09-21 18:12:31 -0300

    refactor: changed dto field to username

--- src/main/java/br/app/visuwall/user/dto/UserResponse.java
+++ src/main/java/br/app/visuwall/user/dto/UserResponse.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 public record UserResponse(
         UUID id,
         String email,
-        String name,
+        String username,
         Instant createdAt
 ) {
     public static UserResponse from(User user) {
